1e1d1eea9d9a9e71225213cffd86fc2a778064e1
abhishek <abhishek.nt94@gmail.com>
2023-02-16 08:42:53 -0800

    JSON

---
+++ jsonNode/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const bioData ={
+    name: "abhishek",
+    age: 21,
+    channel: "abhisays",
+};
+
+// const jsonData = JSON.stringify(bioData);
+// const objData = JSON.parse(jsonData);
+// console.log(objData)
+
+const jsonData = JSON.stringify(bioData);
+fs.writeFile('json1.json',jsonData, (err) =>{
+   // console.log("data")
+   const orgData = JSON.parse(done);
+   console.log(orgData);
+})
+
+fs.readFile("json1.json", "utf-8",(err,data)=> {
+    console.log("data")
+})
