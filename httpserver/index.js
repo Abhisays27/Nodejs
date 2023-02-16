1323d0a7ceb9bda05f3f7599b5a29961d23edf13
abhishek <abhishek.nt94@gmail.com>
2023-02-16 08:23:03 -0800

    routing

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,9 +1,24 @@
 const http =  require('node:http');
+const { type } = require('node:os');
 const server = http.createServer((req, res) => {
-    res.end('hello from the other sides');
+    if(req.url == "/"){
+        res.end('hello from the home sides');
+     }
+     else if(req.url == "/about"){
+        res.end("hello from aboutus")
+     }
+     else if(req.url == "/contact"){
+        res.end("hello from contactus")
+     }
+     else{
+        res.writeHead(404, {"content-type" : "text/html"} )
+        res.end("<h1>Error 404! Page not found</h1>")
+     }
+    
 });
 
 server.listen(8000, "127.0.0.1", ()=>
 {
+    
     console.log('listening to the port no 8000');
 });
